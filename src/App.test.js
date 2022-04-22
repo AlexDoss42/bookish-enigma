a0af65093b0866a4f2c9299a06f578ffd017d7fb
AlexDoss42 <alex.doss42@gmail.com>
2022-04-21 18:45:34 -0600

    adds a few more tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 test("inputs should be initially empty", () => {
@@ -9,4 +10,27 @@ test("inputs should be initially empty", () => {
   expect(emailInputElement.value).toBe("");
   expect(passwordInputElement).toHaveValue("");
   expect(confirmPasswordInputElement).toHaveValue("");
+});
+
+test("should be able to type an email", () => {
+  render(<App />);
+  const emailInputElement = screen.getByRole("textbox", {
+    name: /email/i
+  });
+  userEvent.type(emailInputElement, "test@gmail.com");
+  expect(emailInputElement).toHaveValue("test@gmail.com");
+});
+
+test("should be able to type a password", () => {
+  render(<App />);
+  const passwordInputElement = screen.getByLabelText("Password");
+  userEvent.type(passwordInputElement, "strong");
+  expect(passwordInputElement).toHaveValue("strong");
+});
+
+test("should be able to type a confirm password", () => {
+  render(<App />);
+  const confirmPasswordInputElement = screen.getByLabelText(/confirm password/i);
+  userEvent.type(confirmPasswordInputElement, "strong");
+  expect(confirmPasswordInputElement).toHaveValue("strong");
 });
